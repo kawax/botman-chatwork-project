1784d016355828f347c573d168e1d227848c3e87
kawax <kawaxbiz@gmail.com>
2017-12-17 23:04:21 +0900

    wip

--- lib/botman-chatwork/src/ChatWorkDriver.php
+++ lib/botman-chatwork/src/ChatWorkDriver.php
@@ -55,7 +55,7 @@ class ChatWorkDriver extends HttpDriver
     {
         info($this->payload->get('webhook_event_type'));
 
-        return $this->validateSignature() && $this->payload->get('webhook_event_type') === 'message_created';
+        return $this->payload->get('webhook_event_type') === 'message_created';
     }
 
     /**
